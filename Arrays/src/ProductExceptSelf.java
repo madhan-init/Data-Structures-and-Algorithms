49aa40dddc89757ee347d43493d6e54ec1004217
madhanraghuram <madhanraghuram@gmail.com>
2026-02-18 21:22:23 +0530

    Added in Arrays

---
+++ Arrays/src/ProductExceptSelf.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class ProductExceptSelf {
+
+    public static int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        int prefix = 1;
+        for (int i = 0; i < n; i++) {
+            res[i] = prefix;
+            prefix *= nums[i];
+        }
+        int suffix = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] *= suffix;
+            suffix *= nums[i];
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+
+        int[] result = productExceptSelf(nums);
+
+        System.out.println("Input  : " + Arrays.toString(nums));
+        System.out.println("Output : " + Arrays.toString(result));
+    }
+}
