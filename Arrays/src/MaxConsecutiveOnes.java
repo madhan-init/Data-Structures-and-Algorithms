c04dc7f4d7713b96540e62fbf685395c45b2c117
madhanraghuram <madhanraghuram@gmail.com>
2026-02-19 21:39:04 +0530

    Added in Arrays

---
+++ Arrays/src/MaxConsecutiveOnes.java
@@ -0,0 +1,20 @@
+public class MaxConsecutiveOnes {
+    public static void main(String[] args) {
+
+        int[] nums = {1, 1, 0, 1, 1, 1};
+
+        int count = 0;
+        int max = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                count++;
+                max = Math.max(max, count);
+            } else {
+                count = 0;
+            }
+        }
+
+        System.out.println("Maximum consecutive ones: " + max);
+    }
+}
