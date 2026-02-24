40f1de16e3e09a62515c04cafc29587043c37de5
madhanraghuram <madhanraghuram@gmail.com>
2026-02-24 22:49:58 +0530

    Added in Arrays

---
+++ Arrays/src/sub_sum_eq_k.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class sub_sum_eq_k {
+    public static int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int sum = 0, count = 0;
+
+        for (int n : nums) {
+            sum += n;
+            if (map.containsKey(sum - k)) {
+                count += map.get(sum - k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int k = sc.nextInt();
+
+        System.out.println(subarraySum(nums, k));
+    }
+}
