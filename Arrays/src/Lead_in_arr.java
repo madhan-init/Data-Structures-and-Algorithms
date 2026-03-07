6a501d900150d9854cf0f3a3dcf1c118f0be3aad
madhanraghuram <madhanraghuram@gmail.com>
2026-03-07 20:17:13 +0530

    Added in arrays

---
+++ Arrays/src/Lead_in_arr.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Lead_in_arr {
+
+        public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+
+            for(int i = 0; i < n; i++){
+                arr[i] = sc.nextInt();
+            }
+
+            int max = arr[n-1];
+            System.out.print(max + " ");
+
+            for(int i = n-2; i >= 0; i--){
+                if(arr[i] > max){
+                    max = arr[i];
+                    System.out.print(max + " ");
+                }
+            }
+        }
+
+}
