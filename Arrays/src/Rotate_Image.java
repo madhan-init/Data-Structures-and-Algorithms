6d830dc15930fae616bbcf023e302d0e1ee6b2eb
madhanraghuram <madhanraghuram@gmail.com>
2026-03-15 22:51:53 +0530

    Added in arrays

---
+++ Arrays/src/Rotate_Image.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+public class Rotate_Image {
+
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+
+        int n = matrix.length;
+
+        // Step 1: Transpose
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Step 2: Reverse each row
+        for(int i = 0; i < n; i++){
+            int left = 0;
+            int right = n - 1;
+
+            while(left < right){
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+
+        // Print matrix
+        for(int[] row : matrix){
+            System.out.println(Arrays.toString(row));
+        }
+    }
+}
