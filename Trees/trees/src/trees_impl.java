e9d1c1ab4d34b99acdc8b7e84f46969eec388379
madhanraghuram <madhanraghuram@gmail.com>
2026-04-10 10:06:18 +0530

    Added to trees

--- Trees/trees/src/trees_impl.java
+++ Trees/trees/src/trees_impl.java
@@ -1,5 +1,3 @@
-import java.util.TreeSet;
-
 class TreeNode{
     int data;
     TreeNode left;
@@ -47,7 +45,43 @@ public class trees_impl {
         preorder(root.left);
         preorder(root.right);
     }
-
+static TreeNode delete(TreeNode root,int data){
+        if(root ==null){
+            return root;
+        }
+        if(data< root.data){
+            root.left=delete(root.left,data);
+        } else if (data> root.data) {
+            root.right=delete(root.right,data);
+        }else {
+            if(root.left==null){
+                return root.right;
+            } else if (root.right==null) {
+                return root.left;
+            }
+            TreeNode temp=findmin(root.right);
+            root.data= temp.data;
+            root.right=delete(root.right, temp.data);
+        }
+        return root;
+}
+static TreeNode findmin(TreeNode root){
+        while (root.left!=null){
+            root=root.left;
+        }
+        return root;
+}
+static void displayleaf(TreeNode root){
+        if (root==null){
+            return;
+        }
+        if (root.right==null && root.left==null){
+            System.out.println(root.data);
+            return;
+        }
+        displayleaf(root.left);
+        displayleaf(root.right);
+}
     static void main(String[] args) {
     trees_impl tr=new trees_impl();
         tr.root=insert(tr.root,5);
@@ -55,6 +89,10 @@ public class trees_impl {
         tr.root=insert(tr.root,1);
         tr.root=insert(tr.root,7);
         tr.root=insert(tr.root,8);
-        tr.postorder(root);
+//        tr.postorder(root);
+        tr.delete(tr.root,7);
+//        tr.inorder(root);
+        tr.displayleaf(tr.root);
+
     }
 }
